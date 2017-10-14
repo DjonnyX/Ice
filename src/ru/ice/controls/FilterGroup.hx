d16655d426e75f9a14733c73f52276edb61fa012
djonnyx <djonnyx@gmail.com>
2017-10-14 20:41:23 +0300

    Optimization for fast animation and rendering

--- src/ru/ice/controls/FilterGroup.hx
+++ src/ru/ice/controls/FilterGroup.hx
@@ -1,6 +1,7 @@
 package ru.ice.controls;
 
 import haxe.Constraints.Function;
+import ru.ice.controls.super.BaseListItemControl;
 
 import ru.ice.animation.IAnimatable;
 import ru.ice.animation.Transitions;
@@ -22,9 +23,7 @@ class FilterGroup extends IceControl
 	public static inline var DEFAULT_STYLE:String = 'default-filter-group-style';
 	
 	private var _layoutTween:IAnimatable;
-	private var _layoutTween1:IAnimatable;
 	private var _layoutTween2:IAnimatable;
-	private var _layoutTween3:IAnimatable;
 	
 	private var _rightOffset:Float = 0;
 	public var rightOffset(get, set) : Float;
@@ -32,8 +31,7 @@ class FilterGroup extends IceControl
 		if (_rightOffset != v) {
 			_rightOffset = v;
 			removeLayoutAnimation();
-			_layoutTween = Ice.animator.tween(_container, .125, {marginRight:v + _openButton.width, transition:Transitions.EASE_OUT});
-			_layoutTween1 = Ice.animator.tween(_openButtonContainer.layout, .125, {paddingRight:v, transition:Transitions.EASE_OUT});
+			_layoutTween = Ice.animator.tween(layout, .125, {paddingRight:v, transition:Transitions.EASE_OUT, onUpdate:updateTween});
 		}
 		return get_rightOffset();
 	}
@@ -48,10 +46,13 @@ class FilterGroup extends IceControl
 		if (_leftOffset != v) {
 			_leftOffset = v;
 			removeLayoutAnimation1();
-			_layoutTween2 = Ice.animator.tween(_container, .4, {x:v, marginLeft:v, transition:Transitions.EASE_OUT});
-			_layoutTween3 = Ice.animator.tween(_openButtonContainer.layout, .35, {paddingLeft:v, transition:Transitions.EASE_OUT});
+			_layoutTween2 = Ice.animator.tween(layout, .30, {paddingLeft:v, transition:Transitions.EASE_OUT, onUpdate:updateTween});
 		}
 		return get_leftOffset();
+	}
+	
+	private function updateTween() : Void {
+		update();
 	}
 	
 	private function get_leftOffset() : Float {
@@ -61,7 +62,6 @@ class FilterGroup extends IceControl
 	private var _openButton:Button;
 	private var _buttonsGroup:ButtonsGroup;
 	private var _container:IceControl;
-	private var _openButtonContainer:IceControl;
 	
 	private var _buttonsGroupStyleFactory:Function;
 	public var buttonsGroupStyleFactory(get, set) : Function;
@@ -105,20 +105,6 @@ class FilterGroup extends IceControl
 		return _containerStyleFactory;
 	}
 	
-	private var _openButtonContainerStyleFactory:Function;
-	public var openButtonContainerStyleFactory(get, set) : Function;
-	private function set_openButtonContainerStyleFactory(v:Function) : Function {
-		if (_openButtonContainerStyleFactory != v) {
-			_openButtonContainerStyleFactory = v;
-			if (_openButtonContainer != null)
-				_openButtonContainer.styleFactory = v;
-		}
-		return get_openButtonContainerStyleFactory();
-	}
-	private function get_openButtonContainerStyleFactory() : Function {
-		return _openButtonContainerStyleFactory;
-	}
-	
 	private var _accessoryItems:Array<Dynamic>;
 	public var accessoryItems(get, set):Array<Dynamic>;
 	private function set_accessoryItems(v:Array<Dynamic>) : Array<Dynamic> {
@@ -159,29 +145,37 @@ class FilterGroup extends IceControl
 	
 	override public function initialize() : Void 
 	{
-		_openButtonContainer = new IceControl(new ElementData({'interactive':false}));
-		_openButtonContainer.styleFactory = _openButtonContainerStyleFactory;
-		addChild(_openButtonContainer);
-		_openButton = new Button();
-		_openButton.isToggle = true;
-		_openButton.addEventListener(Event.TRIGGERED, triggeredHandler);
-		_openButton.styleFactory = _openButtonStyleFactory;
-		_openButtonContainer.addChild(_openButton);
-		
 		_container = new IceControl(new ElementData({'interactive':false}));
 		_container.styleFactory = _containerStyleFactory;
 		addChild(_container);
-		_buttonsGroup = new ButtonsGroup();
+		_buttonsGroup = new ButtonsGroup(new ElementData({'interactive':false}));
+		_buttonsGroup.addEventListener(Event.CHANGE, buttonsGroupTriggeredHandler);
 		_buttonsGroup.addEventListener(Event.RESIZE, buttonsGroupResizeHandler);
 		_buttonsGroup.styleFactory = _buttonsGroupStyleFactory;
 		if (_accessoryItems != null)
 			_buttonsGroup.accessoryItems = _accessoryItems;
 		_container.addChild(_buttonsGroup);
+		
+		_openButton = new Button();
+		_openButton.isToggle = true;
+		_openButton.addEventListener(Event.TRIGGERED, triggeredHandler);
+		_openButton.styleFactory = _openButtonStyleFactory;
+		addChild(_openButton);
 		super.initialize();
 		
 		// start pos
 		_buttonsGroup.x = Ice.screenWidth;
 		_buttonsGroup.visible = _collapsed;
+	}
+	
+	private function buttonsGroupTriggeredHandler(e:Event, data:Dynamic) : Void {
+		e.stopImmediatePropagation();
+		var items:Array<Dynamic> = cast data;
+		var tags:Array<String> = [];
+		for (item in items) {
+			tags.push(item.tag);
+		}
+		dispatchEventWith(Event.CHANGE, true, tags);
 	}
 	
 	private function buttonsGroupResizeHandler() : Void {
@@ -209,9 +203,8 @@ class FilterGroup extends IceControl
 	}
 	
 	private function triggeredHandler(e:Event) : Void {
-		if (_openButton.isSelect) {
+		if (_openButton.isSelect)
 			_buttonsGroup.visible = true;
-		}
 		collapsed = _openButton.isSelect;
 	}
 	
@@ -220,10 +213,6 @@ class FilterGroup extends IceControl
 			Ice.animator.remove(_layoutTween);
 			_layoutTween = null;
 		}
-		if (_layoutTween1 != null) {
-			Ice.animator.remove(_layoutTween1);
-			_layoutTween1 = null;
-		}
 	}
 	
 	private function removeLayoutAnimation1() : Void {
@@ -231,20 +220,17 @@ class FilterGroup extends IceControl
 			Ice.animator.remove(_layoutTween2);
 			_layoutTween2 = null;
 		}
-		if (_layoutTween3 != null) {
-			Ice.animator.remove(_layoutTween3);
-			_layoutTween3 = null;
-		}
 	}
 	
 	public override function dispose() : Void {
-		_openButtonContainerStyleFactory = null;
 		_containerStyleFactory = null;
 		_buttonsGroupStyleFactory = null;
 		_openButtonStyleFactory = null;
 		removeLayoutAnimation();
 		removeLayoutAnimation1();
 		if (_buttonsGroup != null) {
+			_buttonsGroup.removeEventListener(Event.RESIZE, buttonsGroupResizeHandler);
+			_buttonsGroup.removeEventListener(Event.CHANGE, buttonsGroupTriggeredHandler);
 			_buttonsGroup.removeFromParent(true);
 			_buttonsGroup = null;
 		}
@@ -253,12 +239,9 @@ class FilterGroup extends IceControl
 			_container = null;
 		}
 		if (_openButton != null) {
+			_openButton.addEventListener(Event.TRIGGERED, triggeredHandler);
 			_openButton.removeFromParent(true);
 			_openButton = null;
-		}
-		if (_openButtonContainer != null) {
-			_openButtonContainer.removeFromParent(true);
-			_openButtonContainer = null;
 		}
 		super.dispose();
 	}
