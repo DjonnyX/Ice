374f1ebd917b1410c34ba5c1a502e11046dec0af
djonnyx <djonnyx@gmail.com>
2017-10-14 20:42:19 +0300

    Changed interface due to optimization

---
+++ src/ru/ice/controls/IButton.hx
@@ -0,0 +1,20 @@
+package ru.ice.controls;
+
+import haxe.Constraints.Function;
+
+import js.html.Element;
+
+import ru.ice.controls.super.IBaseStatesControl;
+
+/**
+ * @author Evgenii Grebennikov
+ */
+interface IButton extends IBaseStatesControl
+{
+	public var labelStyleFactory(get, set) : Function;
+	public var iconStyleFactory(get, set) : Function;
+	public var label(get, set) : String;
+	public var icon(get, set) : Array<String>;
+	public var labelElement(get, never) : Element;
+	public var iconElement(get, never) : Element;
+}
